8313bcd2948745a238bb2ae61a92e0dc16a9747a
Abraham <ap94143@gmail.com>
2026-05-24 08:27:49 +0200

    Add Google Tag Manager

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -32,7 +32,22 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W2XQ9ZZV');`,
+          }}
+        />
+      </head>
       <body className="bg-bg text-text antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W2XQ9ZZV"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           <Navbar />
           <main>{children}</main>
